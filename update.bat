6b33d69268afc07a0ca428fb68f9bc0aa7f24c7c
nelson.mccullough <nelson.mccullough@peloton.com>
2021-04-26 18:47:01 -0700

    changed update.bat to new style

--- update.bat
+++ update.bat
@@ -1,8 +1,5 @@
 @echo off
 
-echo "Started build..."
-node run build
-
 echo "Starting updating..."
 git pull
 git add *
@@ -10,9 +7,3 @@ git add .
 git commit -m "update to website"
 git push
 echo "Finished updating."
-
-@REM echo "Pushing update to server..."
-
-@REM ssh nelr@107.161.26.119 "cd jesseatter.ca && git pull && npm run build"
-
-@REM echo "Update pushed to server!"
