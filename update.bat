14039a955de941f4d718b8fdbb2f06f5a0db7924
nelson.mccullough <nelson.mccullough@peloton.com>
2021-04-26 18:20:40 -0700

    remove old

--- update.bat
+++ update.bat
@@ -11,8 +11,8 @@ git commit -m "update to website"
 git push
 echo "Finished updating."
 
-echo "Pushing update to server..."
+@REM echo "Pushing update to server..."
 
-ssh nelr@107.161.26.119 "cd jesseatter.ca && git pull && npm run build"
+@REM ssh nelr@107.161.26.119 "cd jesseatter.ca && git pull && npm run build"
 
-echo "Update pushed to server!"
+@REM echo "Update pushed to server!"
