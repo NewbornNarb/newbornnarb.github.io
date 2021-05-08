32b448f458a3d2b1c7bd2e40c1dfc54a03f63dd4
Nelson M <nelson.mccullough@peloton.com>
2021-05-08 16:41:19 -0600

    update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,42 +6,35 @@
                 <v-row>
                     <v-col>
                         <v-list color="transparent" dark>
-                            <v-list-item-group>
-                                <v-list-item>
-                                    <h3>Contact</h3>
-                                </v-list-item>
-                                <v-list-item>(587) 585-6581</v-list-item>
-                                <v-list-item>j.atter66@gmail.com</v-list-item>
-                                <v-list-item>Resume</v-list-item>
-                            </v-list-item-group>
+                            <v-list-item>
+                                <h3>Contact</h3>
+                            </v-list-item>
+                            <v-list-item>(587) 585-6581</v-list-item>
+                            <v-list-item>j.atter66@gmail.com</v-list-item>
+                            <v-list-item href="/resume.pdf" target="__blank">Resume</v-list-item>
                         </v-list>
                     </v-col>
                     <v-col>
                         <v-list color="transparent" dark>
-                            <v-list-item-group>
                                 <v-list-item>
                                     <h3>Social</h3>
                                 </v-list-item>
-                                <v-list-item>Linkedin</v-list-item>
-                            </v-list-item-group>
+                                <v-list-item href="https://www.linkedin.com/in/jesseatter/" target="__blank">Linkedin</v-list-item>
                         </v-list>
                     </v-col>
                     <v-col>
                         <v-list color="transparent" dark>
-                            <v-list-item-group>
-                                <v-list-item>
-                                    <h3>Links</h3>
-                                </v-list-item>
-                                <v-list-item>Renders</v-list-item>
-                                <v-list-item>Models</v-list-item>
-                                <v-list-item>ArtStation</v-list-item>
-                            </v-list-item-group>
+                            <v-list-item>
+                                <h3>Links</h3>
+                            </v-list-item>
+                            <v-list-item href="/renders">Renders</v-list-item>
+                            <v-list-item href="https://www.artstation.com/jesseatter" target="__blank">ArtStation</v-list-item>
                         </v-list>
                     </v-col>
                 </v-row>
             </v-card-text>
             <center>
-                <small>Copyright 2020 © Jesse Atter. All rights reserved.</small>
+                <small>Copyright {{new Date().getFullYear()}} © Jesse Atter. All rights reserved.</small>
             </center>
         </v-card>
     </v-container>
